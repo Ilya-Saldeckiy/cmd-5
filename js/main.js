30944d28dcd26dc8bf5bf55920c143598832f267
saldetskii_ilia <melllllowtoy@yandex.ru>
2021-03-25 12:54:09 +0300

    add modal

--- js/main.js
+++ js/main.js
@@ -6,9 +6,31 @@ $(function() {
     } );
 });
 
-$(document).on('click', '.video__preview', function() {
-	var $video = $('#video'),
-		src = $video.attr('src');
+$(document).ready(function($) {
+	$('.popup-open').click(function() {
+        $("body").addClass("hidden");
+		$('.popup-fade').fadeIn();
+		return false;
+	});	
+	
+	$('.popup-close').click(function() {
+        $("body").removeClass("hidden");
+		$(this).parents('.popup-fade').fadeOut();
+		return false;
+	});		
  
-	$video.attr('src', src + '&autoplay=1');
+	$(document).keydown(function(e) {
+		if (e.keyCode === 27) {
+			e.stopPropagation();
+			$('.popup-fade').fadeOut();
+		}
+	});
+	
+	$('.popup-fade').click(function(e) {
+        $("body").removeClass("hidden");
+		if ($(e.target).closest('.popup').length == 0) {
+			$(this).fadeOut();					
+		}
+	});
+
 });
